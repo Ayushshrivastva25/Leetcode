10fa642cfd2fbc8197f645b5baeeccfcc5958834
Ayush Shrivastva <ayushshrivastva25@gmail.com>
2025-09-02 23:45:35 +0530

    Time: 0 ms (100%), Space: 8.3 MB (45.49%) - LeetHub

---
+++ 0274-h-index/0274-h-index.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int cmp(const void *a, const void *b) {
+    return (*(int*)b - *(int*)a);
+}
+
+int hIndex(int* citations, int citationsSize) {
+   
+    qsort(citations, citationsSize, sizeof(int), cmp);
+
+    int h = 0;
+    for (int i = 0; i < citationsSize; i++) {
+        if (citations[i] >= i + 1) {
+            h = i + 1;
+        } else {
+            break;
+        }
+    }
+    return h;
+}
