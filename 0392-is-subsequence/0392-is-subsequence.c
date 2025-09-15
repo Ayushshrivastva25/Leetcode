bc040cf8662981db2dd50e28ac1f6dc34c1d6d01
Ayush Shrivastva <ayushshrivastva25@gmail.com>
2025-09-15 09:37:47 +0530

    Time: 0 ms (100%), Space: 7.9 MB (54.87%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.c
+++ 0392-is-subsequence/0392-is-subsequence.c
@@ -6,14 +6,14 @@ bool isSubsequence(char* s, char* t) {
     int lenS = strlen(s);
     int lenT = strlen(t);
 
-    // Traverse both strings
+    
     while (i < lenS && j < lenT) {
         if (s[i] == t[j]) {
-            i++;  // move to next char in s if a match is found
+            i++;  
         }
-        j++;      // always move forward in t
+        j++;      
     }
 
-    // If all characters in s were matched
+
     return i == lenS;
 }
